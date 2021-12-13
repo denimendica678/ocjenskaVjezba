2793603fb1293934a3d34bfb9e59877d0aac6a4f
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-12-13 09:21:30 +0100

    Add files via upload

---
+++ knjiznica2/knjiznica2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace knjiznica2
+{
+    public partial class Form1 : Form
+    {
+        private string contents;
+
+        public object UsersÎnik { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            using (StreamReader streamReader = new StreamReader("C: \Users\ucenik\Downloads", Encoding.UTF8))
+            {
+                contents = streamReader.ReadToEnd();
+            }
+            doc.LoadXml(contents);
+        }
+    }
+}
